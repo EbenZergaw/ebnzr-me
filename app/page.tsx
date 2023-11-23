b7b30efa3c5a3bd47741aaa2d859ff28462319cf
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2023-11-23 14:11:49 -0500

    -

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import portrait from '../public/images/me.png'
+import portrait from '../public/images/me.PNG'
 import code from '../public/images/code.svg'
 import roundhouse from '../public/images/roundhouse.png'
 
