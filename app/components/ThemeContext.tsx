bbc09f5610fd333a1bf0ed78796847bb4db92b22
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2023-11-23 16:02:02 -0500

    Deployment error fix

--- app/components/ThemeContext.tsx
+++ app/components/ThemeContext.tsx
@@ -9,32 +9,48 @@ const ThemeContext = createContext({
 export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }: any) => {
-  const [theme, setTheme] = useState(() => {
-    const storedTheme = localStorage.getItem('theme');
-    return typeof storedTheme === 'string' ? storedTheme : 'dark';
-  });
 
-  const toggleTheme = () => {
-    const newTheme = theme === 'dark' ? 'light' : 'dark';
-    setTheme(newTheme);
-    if (typeof window !== 'undefined') {
-      localStorage.setItem('theme', newTheme);
-    }
-  };
+  if(localStorage !== undefined){
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      localStorage.setItem('theme', theme);
-    }
-  }, [theme]);
+    const [theme, setTheme] = useState(() => {
+      const storedTheme = localStorage.getItem('theme');
+      return typeof storedTheme === 'string' ? storedTheme : 'dark';
+    });
+  
+    const toggleTheme = () => {
+      const newTheme = theme === 'dark' ? 'light' : 'dark';
+      setTheme(newTheme);
+      if (typeof window !== 'undefined') {
+        localStorage.setItem('theme', newTheme);
+      }
+    };
+  
+    useEffect(() => {
+      if (typeof window !== 'undefined') {
+        localStorage.setItem('theme', theme);
+      }
+    }, [theme]);
+  
+    return (
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <html data-theme={theme} className={theme}>
+          {children}
+        </html>
+      </ThemeContext.Provider>
+    );
 
-  return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      <html data-theme={theme} className={theme}>
-        {children}
-      </html>
-    </ThemeContext.Provider>
-  );
+  } else {
+    const theme = 'dark'
+    const toggleTheme = () => {}
+    return (
+      <ThemeContext.Provider value={{theme, toggleTheme}}>
+        <html data-theme={'dark'} className={'dark'}>
+          {children}
+        </html>
+      </ThemeContext.Provider>
+    );
+  }
+
 };
 
 export default ThemeProvider;
