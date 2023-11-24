140fcd5848542633f2072d829011c6c613548cf3
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2023-11-23 19:25:29 -0500

    post page fix

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -9,13 +9,16 @@ import Link from 'next/link'
 export async function generateStaticParams(){
 
     const files = fs.readdirSync(path.join('posts'))
+
     const paths = files.map(filename => ({
-        sluge: filename.replace('mdx', '')
+        slug: filename.replace('mdx', '')
     }))
+
     return paths
 }
 
 function getPost({slug}: {slug: string}){
+
     const markdownFile = fs.readFileSync(path.join('posts', slug + '.mdx'), 'utf-8')
     
     const {data: frontmatter, content} = matter(markdownFile)
@@ -27,7 +30,9 @@ function getPost({slug}: {slug: string}){
 }
 
 export async function generateMetadata({ params }: { params: { slug: string } }) {
+  
     const props = getPost(params)
+
     return {
       title: props.frontmatter.title,
       description: props.frontmatter.description
