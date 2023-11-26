bb849a73afddf458a4b03b35434d65d6486cb399
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2023-11-26 11:53:15 -0500

    Skills List fix

--- app/components/SkillsList.tsx
+++ app/components/SkillsList.tsx
@@ -1,22 +1,16 @@
 
-import { MDXRemote } from 'next-mdx-remote/rsc'
 import React from 'react'
 
 function SkillsList() {
 
-    // const skills = `[![My Skills](https://skillicons.dev/icons?i=js,html,css,typescript,react,nextjs,firebase,ps,prisma,tailwind,vscode,nodejs&perline=6)](https://skillicons.dev)`
+    const skills = `https://skillicons.dev/icons?i=js,html,css,typescript,react,nextjs,firebase,ps,prisma,tailwind,vscode,nodejs&perline=6`
 
-    const skills = {
-        markdown: "[![My Skills](https://skillicons.dev/icons?i=js,html,css,typescript,react,nextjs,firebase,ps,prisma,tailwind,vscode,nodejs&perline=6)](https://skillicons.dev)"
-    }
 
   return (
     <div className='w-fit mx-auto lg:mx-0'>
         <h2 className="text-primary font-semibold text-3xl">Skills</h2>
         <br />
-        <MDXRemote
-            source={skills.markdown}>
-        </MDXRemote>
+        <img src={skills}></img>
     </div>
   )
 }
