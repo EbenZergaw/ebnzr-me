9436a7742bfd1a03160d61c2ff6d4947d986e144
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2024-10-23 16:00:05 -0400

    outline

--- app/projects/Guest360.tsx
+++ app/projects/Guest360.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
 
 function Guest360() {
   return (
-    <div>
-      
+    <div className="h-full lg:flex">
+       <h1 className="font-bold text-3xl mb-4">
+            Guest 360
+        </h1>
+
+        <div>
+            Methodology
+            - Research
+            - Hypothesis
+            - Design
+            - Development
+        </div>
+
+        <div>
+            Features
+            - Authentication
+            - Cyber Security and access control
+            - AI Insights
+        </div>
+
+        <div>
+            Tech Stack
+            - Next.js
+            - Tailwind CSS
+            - Shadcn UI
+            - Clerk Authentication
+            - Groq llama 3 
+            - Django / Python / Postgres on Heroku
+        </div>
     </div>
-  )
+  );
 }
 
-export default Guest360
+export default Guest360;
